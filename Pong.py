cca99df351fe4aeeaab221f38b193447914007cd
Joe Panetta <jlapnetta1681@icloud.com>
2020-11-24 14:39:24 -0500

    basic game done working on sound

---
+++ Pong.py
@@ -0,0 +1,133 @@
+# Simple Pong in Python 3
+# By Joe Panetta
+
+import turtle
+import winsound
+
+win = turtle.Screen()
+win.title("Joe's Pong")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Score
+score_1 = 0
+score_2 = 0
+
+# Player1
+player_1 = turtle.Turtle()
+player_1.speed(0)
+player_1.shape("square")
+player_1.color("white")
+player_1.shapesize(stretch_wid=5, stretch_len=1)
+player_1.penup()
+player_1.goto(-350, 0)
+
+# Player_2
+player_2 = turtle.Turtle()
+player_2.speed(0)
+player_2.shape("square")
+player_2.color("white")
+player_2.shapesize(stretch_wid=5, stretch_len=1)
+player_2.penup()
+player_2.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.15
+ball.dy = -0.15
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player 1: 0 Payer 2: 0", align="center", font=("Courier", 24, "normal"))
+
+
+
+# Functions to move paddles
+def player_1_up():
+    y = player_1.ycor()
+    y += 20
+    player_1.sety(y)
+
+
+def player_1_down():
+    y = player_1.ycor()
+    y -= 20
+    player_1.sety(y)
+
+
+def player_2_up():
+    y = player_2.ycor()
+    y += 20
+    player_2.sety(y)
+
+
+def player_2_down():
+    y = player_2.ycor()
+    y -= 20
+    player_2.sety(y)
+
+
+# Keyboard bindings
+
+win.listen()
+win.onkeypress(player_1_up, "w")
+win.onkeypress(player_1_down, "s")
+win.onkeypress(player_2_up, "Up")
+win.onkeypress(player_2_down, "Down")
+
+# Main game
+while True:
+    win.update()
+    
+    # Moving the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Boarder contact
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("Ping-pong-ball-bounce-sound-effect.mp3", winsound.SND_ASYNC)
+    
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("Ping-pong-ball-bounce-sound-effect.mp3", winsound.SND_ASYNC)
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_1 += 1
+        pen.clear()
+        pen.write("Player 1: {} Payer 2: {}".format(score_1, score_2), align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_2 += 1
+        pen.clear()
+        pen.write("Player 1: {} Payer 2: {}".format(score_1, score_2), align="center", font=("Courier", 24, "normal"))
+
+    # Paddles hit the ball
+    if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < player_2.ycor() + 40 and ball.ycor() > player_2.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("Ping-pong-ball-bounce-sound-effect.mp3", winsound.SND_ASYNC)
+
+    if ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < player_1.ycor() + 40 and ball.ycor() > player_1.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("Ping-pong-ball-bounce-sound-effect.mp3", winsound.SND_ASYNC)
+
